845d1268efa21adc66b2be4a5404d8473bf81fca
Hiroki Tanaka <rokitaniplus1@gmail.com>
2024-05-30 10:06:57 +0900

    added Encoders

--- gaga/cga/component.py
+++ gaga/cga/component.py
@@ -1,23 +1,122 @@
-from abc import ABC,abstractmethod
+from abc import ABC
 from typing import List,Sequence
 
-class Base(ABC):
+class Base:
     def __init__(self,id:int,type:str,letter:object) -> None:
-        self.id=id
-        self.type=type
-        self.letter=letter
+        self.setId(id)
+        self.setType(type)
+        self.setLetter(letter)
+    def setId(self,value):
+        self._id=value
+    def setType(self,value):
+        self._type=value
+    def getId(self):
+        return self._id
+    def getType(self):
+        return self._type
+    def getLetter(self):
+        return self._letter
+    def setLetter(self,value):
+        self._letter=value
 
-class Codon(ABC):
+def defBase(className:str,idLabel:str,typeLabel:str,letterLabel:str):
+    NewBase=type(className, (Base,), {})
+    def getX(self):
+        return self._id
+    def setX(self,value):
+        self.setId(value)
+    def getY(self):
+        return self._type
+    def setY(self,value):
+        self.setType(value)
+    def getZ(self):
+        return self._letter
+    def setZ(self,value):
+        self.setLetter(value)
+
+    setattr(NewBase,idLabel,property(getX,setX))
+    setattr(NewBase,typeLabel,property(getY,setY))
+    setattr(NewBase,letterLabel,property(getZ,setZ))
+    newBase=NewBase
+    return newBase
+
+class Codon:
     def __init__(self,id:int,type:str,basis: Sequence[Base]) -> None:
-        self.id=id
-        self.type=type
-        self.basis=basis
+        self.setId(id)
+        self.setType(type)
+        self.setBasis(basis)
+    def setId(self,value):
+        self._id=value
+    def setType(self,value):
+        self._type=value
+    def getId(self):
+        return self._id
+    def getType(self):
+        return self._type
+    def getBasis(self):
+        return self._basis
+    def setBasis(self,value):
+        self._basis=value
+        
+def defCodon(className:str,idLabel,typeLabel,basisLabel) -> Codon:
+    NewCodon=type(className, (Codon,), {})
+    def getX(self):
+        return self._id
+    def setX(self,value):
+        self.setId(value)
+    def getY(self):
+        return self._type
+    def setY(self,value):
+        self.setType(value)
+    def getZ(self):
+        return self._basis
+    def setZ(self,value):
+        self.setBasis(value)
+
+    setattr(NewCodon,idLabel,property(getX,setX))
+    setattr(NewCodon,typeLabel,property(getY,setY))
+    setattr(NewCodon,basisLabel,property(getZ,setZ))
+    newCodon=NewCodon
+    return newCodon
         
 class Individual(ABC):
-    def __init__(self,id: int,gene: List[Codon],score: float):
-        self.id=id
-        self.gene=gene
-        self.score=score
+    def __init__(self,id: int,gene,score: float):
+        self.setId(id)
+        self.setGene(gene)
+        self.setScore(score)
+    def setId(self,value):
+        self._id=value
+    def setGene(self,value):
+        self._gene=value
+    def getId(self):
+        return self._id
+    def getGene(self):
+        return self._gene
+    def getScore(self):
+        return self._score
+    def setScore(self,value):
+        self._score=value    
+
+def defIndividual(className:str,idLabel,geneLabel,scoreLabel) -> Codon:
+    NewIndividual=type(className, (Individual,), {})
+    def getX(self):
+        return self._id
+    def setX(self,value):
+        self.setId(value)
+    def getY(self):
+        return self._gene
+    def setY(self,value):
+        self.setGene(value)
+    def getZ(self):
+        return self._score
+    def setZ(self,value):
+        self.setScore(value)
+
+    setattr(NewIndividual,idLabel,property(getX,setX))
+    setattr(NewIndividual,geneLabel,property(getY,setY))
+    setattr(NewIndividual,scoreLabel,property(getZ,setZ))
+    newIndividual=NewIndividual
+    return newIndividual
   
 class Population:
     def __init__(self,size:int,generation: int,individuals: List[Individual]):
